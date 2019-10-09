a81ca4a2286fc00498d24eb7e9ee4b353bc2a3a8
pranaykumargoud <pranaygoud36@gmail.com>
2019-10-09 18:42:38 +0530

    Add controller

---
+++ spring/src/main/java/com/eurekachat/spring/controller/ChatController.java
@@ -0,0 +1,20 @@
+package com.eurekachat.spring.controller;
+
+import com.eurekachat.spring.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage){
+        return chatMessage;
+    }
+
+}
